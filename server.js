e6800742022830326b02a7a226e1dfba9d46ff57
Danny <jacquez.j.daniel@gmail.com>
2019-03-29 20:45:21 -0700

    server

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 var express = require('express');
+var methodOverride = require('method-override');
+var bodyParser = require('body-parser');
 var exphbs = require('express-handlebars');
-var PORT = process.env.PORT || 3000;
 var app = express();
+var PORT = process.env.PORT || 3000;
 var db = require('./models');
+
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Override with POST having ?_method=DELETE
+app.use(methodOverride('_method'));
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
@@ -11,10 +19,9 @@ app.use(express.static('./public'));
 
 require('./controllers/burgers_controllers.js')(app);
 
-db.sequelize.sync({
-    force: false
-}).then(() => {
-    app.listen(PORT, function() {
-        console.log("App listening on PORT " + PORT);
-    });
+db.sequelize.sync().then(function(){
+  // Initiate the listener.
+  app.listen(PORT, function(){
+    console.log('Listening on port %s ', port);
+  });
 });
