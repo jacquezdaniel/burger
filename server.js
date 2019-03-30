ffd436d85efb9b3f52255dc36c71d2ad86de0cba
Danny <jacquez.j.daniel@gmail.com>
2019-03-29 20:00:03 -0700

    json

--- server.js
+++ server.js
@@ -1,16 +1,8 @@
 var express = require('express');
-var methodOverride = require('method-override');
-var bodyParser = require('body-parser');
 var exphbs = require('express-handlebars');
 var PORT = process.env.PORT || 3000;
 var app = express();
 var db = require('./models');
-
-// Parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// Override with POST having ?_method=DELETE
-app.use(methodOverride('_method'));
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
