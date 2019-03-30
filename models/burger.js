00df903aae1c5dbd7838cd0761c3204b5f9a0e78
Danny <jacquez.j.daniel@gmail.com>
2019-03-29 20:59:07 -0700

    fix

--- models/burger.js
+++ models/burger.js
@@ -1,17 +1,17 @@
 module.exports = function(sequelize, DataTypes){
-    var Burger = sequelize.define('Burger', {
-      burger_name: DataTypes.STRING,
-      devoured: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: 0,
-      }
+  var Burger = sequelize.define('Burger', {
+    burger_name: DataTypes.STRING,
+    devoured: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: 0,
+    }
+  });
+
+  Burger.associate = function(models) {
+    Burger.hasOne(models.Customer, {
+      onDelete: "cascade"
     });
-  
-    Burger.associate = function(models) {
-      Burger.hasOne(models.Customer, {
-        onDelete: "cascade"
-      });
-    };
-  
-    return Burger;
-  }
+  };
+
+  return Burger;
+}
