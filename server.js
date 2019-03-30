5cef4bf4ce9135c9eb7f78bce8a652bea762d9b1
Danny <jacquez.j.daniel@gmail.com>
2019-03-29 19:30:47 -0700

    port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
 var exphbs = require('express-handlebars');
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 var db = require('./models');
 
 // Parse application/x-www-form-urlencoded
